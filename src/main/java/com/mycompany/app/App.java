6a3674eb6f010f51457a558bbf32b22ea332fcfa
student <asbassan@gmail.com>
2021-04-18 15:35:23 +0000

    corrected the problem

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! first change";
+    private final String message = "Hello World!";
 
     public App() {}
 
